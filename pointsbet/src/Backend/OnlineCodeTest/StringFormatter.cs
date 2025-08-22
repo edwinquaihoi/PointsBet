c25045e5ab070e2fc10e021977e5dcd79d7b4627
Edwin Quai Hoi <edwinquaihoi@gmail.com>
2025-08-22 14:18:21 +1000

    file scoped namespace

--- pointsbet/src/Backend/OnlineCodeTest/StringFormatter.cs
+++ pointsbet/src/Backend/OnlineCodeTest/StringFormatter.cs
@@ -1,34 +1,32 @@
 ﻿using System.Text;
+namespace PointsBet.Backend.OnlineCodeTest;
 
-namespace PointsBet.Backend.OnlineCodeTest
+/*
+Improve a block of code as you see fit in C#.
+You may make any improvements you see fit, for example:
+    - Cleaning up code
+    - Removing redundancy
+    - Refactoring / simplifying
+    - Fixing typos
+    - Any other light-weight optimisation
+*/
+public class StringFormatter
 {
 
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
-    public class StringFormatter
+    //Code to improve
+    public static string ToCommaSepatatedList(string[] items, string quote)
     {
+        StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
-        //Code to improve
-        public static string ToCommaSepatatedList(string[] items, string quote)
+        if (items.Length > 1)
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
-
-            if (items.Length > 1)
+            for (int i = 1; i < items.Length; i++)
             {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
+                qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
             }
-
-            return qry.ToString();
         }
+
+        return qry.ToString();
     }
 }
+
